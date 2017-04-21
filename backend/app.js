b6da7ce8acaf968224242f6cacefafed49b17076
Illia Potsepaiev <Illia_Potsepaiev@epam.com>
2017-04-21 14:03:30 +0300

    added environment

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '../frontend/school/dist/')));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
